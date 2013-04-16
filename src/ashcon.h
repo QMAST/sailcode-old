3fcb2edfa5e2fe0e07b8be05b24354056c374976
Stephen C <allgood38@gmail.com>
2013-04-16 15:10:56 -0400

    Making better data structures in the header
    No actual functions have been written, hopinh this will keep things clean

--- src/ashcon.h
+++ src/ashcon.h
@@ -9,11 +9,24 @@
 class ashcon {
     private:
         static const int SUCCESS = 0;
-        static const int COMMAND_BUFFER_LENGTH = 128;
         static const int ECHO = 1;
         Stream* line_in;
 
+        // The internal command buffer, think get_line and where it puts
+        // the results right away
+        static const int COMMAND_BUFFER_LENGTH = 128;
         char* command_buffer;
+
+        // Command arguments
+        // NOTE: Specific documentation needed for this
+        static const int COMMAND_ARG_NUM_MAX = 12;
+        char* command_arg_list[COMMAND_ARG_NUM_MAX];
+        int command_arg_num; // Acts as index to arg list
+
+        int command_arg_append();
+        int command_arg_clear();
+
+
 
     public:
         ashcon(Stream* new_line_in);
