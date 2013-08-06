fb4bf1f682a48908133182438ac63507308017d2
Kevin Gibson <rkevingibson@gmail.com>
2013-08-06 17:08:14 -0400

    Finished up satellite class, should be tested tomorrow

--- Pi/lib/satellite.h
+++ Pi/lib/satellite.h
@@ -5,9 +5,38 @@
 #include <string>
 #include <path>
 #include <termios.h>
+#include <vector>
+//Enum containing all the error codes that are currently supported. More will be added
+enum ErrorCode {
+	//Airmar errors
+	AMNR = 0, //Airmar not responsive
+	AMIWV = 1, //
+	AMPBT = 2,
+
+	//Compass errors
+	CNR = 64, //Compass not responsive
+	CEPV = 65, //Extreme pitch value
+	CERV = 66, //Extreme roll value
+	CEDV = 67,  //Extreme dip value
+
+	//AIS Errors
+	AISNR = 128,//AIS not responsive
+
+	//Generic Errors/messages.
+	PIRW = 192, //Reached waypoint succesfully
+	PIPW = 193, //Passed Waypoint without reaching it 
+	PIRD = 194, //Reached destination
+	PISR = 195 //System rebooted
+}
+
 
 class Satellite : public Serial{
-	public:
+public:
 	Satellite(const std::string& path);
-	int sendMessage(const std::string &msg);
+	int sendMessage(float lat, float lon, float avgWind, 
+		short int windDir, short int heading, float peakWind);
+	void addError(ErrorCode err, char* payload);
+private:
+	int send(const char[] msg, int length);
+	std::vector<char> errList;
 };
--- Pi/src/satellite.cpp
+++ Pi/src/satellite.cpp
@@ -6,12 +6,13 @@ Satellite::Satellite(const std::string &path) {
 	if(stat!=0) {
 		Logging::error(__func__, "Unable to set up the Satellite Communicator.");
 	}
+	this->errList();
 }
 
-int Satellite::sendMessage(const std::string &msg) {
+int Satellite::send(const char[] msg, int length) {
 	//Compute checksum
 	unsigned int checksum = 0;
-	for(int i; i<msg.length();i++) {
+	for(int i; i<length; i++) {
 		checksum += (unsigned int) msg[i];
 	}
 	//Extract the two bytes needed.
@@ -26,6 +27,89 @@ int Satellite::sendMessage(const std::string &msg) {
 	this->sendCommand(cmd, ready);
 
 	//Send message followed by two-byte checksum.
-	write(this->fildes, msg.c_str(), msg.length());
+	write(this->fildes, msg.c_str(), length);
 	write(this->fildes, chk, 2);
+}
+
+void Satellite::addError(ErrorCode err, char* payload) {
+	//Add the error code as a char to the errList;
+	char errCode = (char) err;
+	this->errList.push_back(errCode);
+
+	if(payload!=NULL) {
+		switch(err) {
+			case CEPV:
+			case CERV:
+			case CEDV:
+				//Payload length of 2.
+				this->errList.push_back(payload[0]);
+				this->errList.push_back(payload[1]);
+
+			break;
+			default:
+				//Some unknown error code. could cause issues.
+			break;
+		}
+	}
+	return;
+}
+
+int Satellite::sendMessage(float lat, float lon, float avgWind,
+				short int windDir, short int heading, float peakWind) {
+	//Construct the byte stream, and determine the length.
+	int size = (20 + this->errList.size());
+	int length=0;
+	//Want to round size to the nearest multiple of 50.
+	if(size/50 > 6) {//More than 300 bytes
+		length = 300;
+	} else {
+		length = size - (size % 50);
+	}
+	char* msg = new char[length];//Might as well make it this long, can expand if necessary to 100 or 150, or so on.
+	
+	memcpy(msg, &lat, 4);
+	memcpy(&(msg[4]), &lon, 4);
+	memcpy(&(msg[8]), &avgWind,4);
+	memcpy(&(msg[12]), &windDir, 2);
+	memcpy(&(msg[14]), &heading, 2);
+	memcpy(&(msg[16]), &peakWind, 4);
+
+	size = 20;
+	//Now the first 20 bytes should be filled properly.
+	//The tricky bit is to not stop sending in the midst of a payload. That'd be bad.
+	//So we loop through the list, adding bits to the buffer.
+	//If we see one of the bits that has a payload, we add it if there's room, or
+	//remove the error and finish sending if there isn't
+	char temp;
+	int nextSize=1;
+	for(int i=0; i<this->errList.size(); i++) {
+		temp = this->errList[i];
+		switch(temp) {
+			case CEPV:
+			case CERV:
+			case CEDV:
+				nextSize = 3;
+			break;
+			default:
+				nextSize = 1;
+			break;
+		}
+
+		if(length - i < nextSize) {
+			break; //Out of space, we're done here.
+		}
+		for(int j =0; j<nextSize; j++) {
+			msg[size] = this->errList[i];
+			i++;
+			size++;
+		}
+	}
+
+	//Send the message
+	send(msg, size);
+
+	//Clear the error list, saving everything past size.
+	this->errList.erase(this->errList.begin(), this->errList.begin()+size);
+	
+	delete[] msg;
 }
